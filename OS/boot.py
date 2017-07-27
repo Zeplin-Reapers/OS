01a887f11efa92f813472f77cc2bf04fb14fce2d
Zeplin-Reapers <30357229+Zeplin-Reapers@users.noreply.github.com>
2017-07-26 22:26:53 -0700

    Update boot.py
    Made it do somthing

--- OS/boot.py
+++ OS/boot.py
@@ -1,4 +1,5 @@
 import time
+import tkinter as tk
 version = "alpha 0"
 full = "false"
 print("OS VERSION: " + version + ";")
@@ -12,4 +13,26 @@ time.sleep(1)
 print(".")
 print("complete!")
 time.sleep(0.5)
-print("ERR_CODE_IS_FALSE")
+class Application(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.pack()
+        self.create_widgets()
+
+    def create_widgets(self):
+        self.hi_there = tk.Button(self)
+        self.hi_there["text"] = "Hello World\n(click me)"
+        self.hi_there["command"] = self.say_hi
+        self.hi_there.pack(side="top")
+
+        self.quit = tk.Button(self, text="QUIT", fg="red",
+                              command=root.destroy)
+        self.quit.pack(side="bottom")
+
+    def say_hi(self):
+        print("hi there, everyone!")
+
+root = tk.Tk()
+app = Application(master=root)
+app.mainloop()
+

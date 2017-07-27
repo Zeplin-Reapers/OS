5e12fa1a09885ae3730ce674a86c3d04ceb1711d
Zeplin-Reapers <30357229+Zeplin-Reapers@users.noreply.github.com>
2017-07-27 13:27:56 -0700

    Update boot.py

--- OS/boot.py
+++ OS/boot.py
@@ -1,6 +1,8 @@
 import time
 import tkinter as tk
-import sys
+import os
+import random
+import math
 version = "alpha 1.0"
 full = "false"
 print("OS VERSION: " + version + ";")
@@ -36,6 +38,21 @@ class Application(tk.Frame):
         self.b3["command"] = self.number3
         self.b3.pack(side="left")
 
+        self.b4 = tk.Button(self, bg="blue")
+        self.b4["text"] = "delete file infromation"
+        self.b4["command"] = self.number5
+        self.b4.pack(side="left")
+
+        self.b5 = tk.Button(self, bg="silver")
+        self.b5["text"] = "Games"
+        self.b5["command"] = self.number4
+        self.b5.pack(side="left")
+
+        self.b6 = tk.Button(self, bg="teal")
+        self.b6["text"] = "Calculator"
+        self.b6["command"] = self.number6
+        self.b6.pack(side="left")
+
         self.quit = tk.Button(self, text="QUIT", bg="red",
                               command=root.destroy)
         self.quit.pack(side="right")
@@ -54,7 +71,30 @@ class Application(tk.Frame):
         val = input("Your Text: ")
         file = open("file/file.txt", "a")
         file.write(val)
-
+    def number4(self):
+        print("Games: pick a number")
+        game = input("Pick a game: ")
+        if "number" in game:
+            num = random.randint(1,100)
+            print(num)
+            guess = input("Pick a number 1 to 100: ")
+            while int(guess) != num:
+               guess = input("guess again: ")
+            print("Congrats! You Won!")
+    def number5(self):
+        file00 = open("file/file.txt", "w")
+        file00.write("")
+    def number6(self):
+        n1 = int(input("first number: "))
+        n2 = int(input("second number: "))
+        print("The options for the operater are:")
+        print(" power")
+        print(" square root")
+        op = input("operator: ")
+        if "power" in  op:
+            print(math.pow(n1,n2))
+        elif "square root" in op:
+            print(math.sqrt(n1))
 root = tk.Tk()
 app = Application(master=root)
 app.mainloop()

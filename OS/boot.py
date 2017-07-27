670cd183ca12de05959c4b00177b1db56533acaf
Zeplin-Reapers <30357229+Zeplin-Reapers@users.noreply.github.com>
2017-07-27 10:46:08 -0700

    Update boot.py

--- OS/boot.py
+++ OS/boot.py
@@ -1,7 +1,7 @@
 import time
 import tkinter as tk
 import sys
-version = "alpha 0"
+version = "alpha 1.0"
 full = "false"
 print("OS VERSION: " + version + ";")
 print("FULL REALESE: " + full + ";")
@@ -13,7 +13,6 @@ print(".")
 time.sleep(1)
 print(".")
 print("complete!")
-time.sleep(0.5)
 count = 1
 class Application(tk.Frame):
     def __init__(self, master=None):
@@ -22,22 +21,22 @@ class Application(tk.Frame):
         self.create_widgets()
 
     def create_widgets(self):
-        self.b1 = tk.Button(self, fg="blue")
+        self.b1 = tk.Button(self, bg="gold")
         self.b1["text"] = "Welcome"
         self.b1["command"] = self.number
         self.b1.pack(side="left")
 
-        self.b2 = tk.Button(self, fg="blue")
+        self.b2 = tk.Button(self, bg="blue")
         self.b2["text"] = "read file"
         self.b2["command"] = self.number2
         self.b2.pack(side="left")
 
-        self.b3 = tk.Button(self, fg="blue")
+        self.b3 = tk.Button(self, bg="blue")
         self.b3["text"] = "write to a file"
         self.b3["command"] = self.number3
         self.b3.pack(side="left")
 
-        self.quit = tk.Button(self, text="QUIT", fg="red",
+        self.quit = tk.Button(self, text="QUIT", bg="red",
                               command=root.destroy)
         self.quit.pack(side="right")
     
@@ -47,13 +46,12 @@ class Application(tk.Frame):
         print("here you have a small OS that is hosted on github.")
         time.sleep(0.5)
         print("https://www.github.com/zeplin-reapers/OS")
-        time.sleep(4)
         print("\n" * 5)
     def number2(self):
         file = open("file/file.txt", "r")
         print(file.read())
     def number3(self):
-        val = input("Your Text")
+        val = input("Your Text: ")
         file = open("file/file.txt", "a")
         file.write(val)
 

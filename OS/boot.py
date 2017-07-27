ac802dc06e3d3ef801817530edfb4c496440ceec
Zeplin-Reapers <30357229+Zeplin-Reapers@users.noreply.github.com>
2017-07-26 22:14:57 -0700

    Add files via upload

---
+++ OS/boot.py
@@ -0,0 +1,15 @@
+import time
+version = "alpha 0"
+full = "false"
+print("OS VERSION: " + version + ";")
+print("FULL REALESE: " + full + ";")
+print("Loading")
+time.sleep(1)
+print(".")
+time.sleep(1)
+print(".")
+time.sleep(1)
+print(".")
+print("complete!")
+time.sleep(0.5)
+print("ERR_CODE_IS_FALSE")

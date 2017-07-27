05a6720cb29e4c9d2935d263ce4f1d483d32dd04
Zeplin-Reapers <30357229+Zeplin-Reapers@users.noreply.github.com>
2017-07-26 23:40:50 -0700

    Update boot.py

--- OS/boot.py
+++ OS/boot.py
@@ -1,5 +1,6 @@
 import time
 import tkinter as tk
+import sys
 version = "alpha 0"
 full = "false"
 print("OS VERSION: " + version + ";")
@@ -13,6 +14,7 @@ time.sleep(1)
 print(".")
 print("complete!")
 time.sleep(0.5)
+count = 1
 class Application(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
@@ -20,19 +22,41 @@ class Application(tk.Frame):
         self.create_widgets()
 
     def create_widgets(self):
-        self.hi_there = tk.Button(self)
-        self.hi_there["text"] = "Hello World\n(click me)"
-        self.hi_there["command"] = self.say_hi
-        self.hi_there.pack(side="top")
+        self.b1 = tk.Button(self, fg="blue")
+        self.b1["text"] = "Welcome"
+        self.b1["command"] = self.number
+        self.b1.pack(side="left")
+
+        self.b2 = tk.Button(self, fg="blue")
+        self.b2["text"] = "read file"
+        self.b2["command"] = self.number2
+        self.b2.pack(side="left")
+
+        self.b3 = tk.Button(self, fg="blue")
+        self.b3["text"] = "write to a file"
+        self.b3["command"] = self.number3
+        self.b3.pack(side="left")
 
         self.quit = tk.Button(self, text="QUIT", fg="red",
                               command=root.destroy)
-        self.quit.pack(side="bottom")
-
-    def say_hi(self):
-        print("hi there, everyone!")
+        self.quit.pack(side="right")
+    
+    def number(self):
+        print("Welcome to the Crystalyte OS!")
+        time.sleep(0.5)
+        print("here you have a small OS that is hosted on github.")
+        time.sleep(0.5)
+        print("https://www.github.com/zeplin-reapers/OS")
+        time.sleep(4)
+        print("\n" * 5)
+    def number2(self):
+        file = open("file/file.txt", "r")
+        print(file.read())
+    def number3(self):
+        val = input("Your Text")
+        file = open("file/file.txt", "a")
+        file.write(val)
 
 root = tk.Tk()
 app = Application(master=root)
 app.mainloop()
-
